4b2fe4db39f0cc7180c92b4ebb7d1747d3d1c90d
jeremyzilar <jeremyzilar@gmail.com>
2023-09-13 11:49:20 -0400

    update to use uswds/compile

--- gulpfile.js
+++ gulpfile.js
@@ -1,144 +1,28 @@
-/*
-* * * * * ==============================
-* * * * * ==============================
-* * * * * ==============================
-* * * * * ==============================
-========================================
-========================================
-========================================
-----------------------------------------
-USWDS SASS GULPFILE
-----------------------------------------
-*/
+/* gulpfile.js */
 
-var autoprefixer  = require('autoprefixer');
-var autoprefixerOptions = require('./node_modules/uswds-gulp/config/browsers');
-var cssnano       = require('cssnano');
-var gulp          = require('gulp');
-// var mqpacker      = require('css-mqpacker');
-var notify        = require('gulp-notify');
-var path          = require('path');
-var pkg           = require('./node_modules/uswds/package.json');
-var postcss       = require('gulp-postcss');
-var rename        = require('gulp-rename');
-var replace       = require('gulp-replace');
-var sass          = require('gulp-sass');
-var sourcemaps    = require('gulp-sourcemaps');
-var uswds         = require('./node_modules/uswds-gulp/config/uswds');
+const uswds = require("@uswds/compile");
 
-/*
-----------------------------------------
-PATHS
-----------------------------------------
-- All paths are relative to the
-  project root
-- Don't use a trailing `/` for path
-  names
-----------------------------------------
-*/
+/**
+ * USWDS version
+ */
 
-// Project Sass source directory
-const PROJECT_SASS_SRC = './assets/sass';
+uswds.settings.version = 3;
 
-// Images destination
-const IMG_DEST = './assets/img';
+/**
+ * Path settings
+ * Set as many as you need
+ */
 
-// Fonts destination
-const FONTS_DEST = './assets/fonts';
+uswds.paths.dist.css = './assets/css';
+uswds.paths.dist.theme = './assets/sass';
+uswds.paths.dist.fonts = './assets/fonts';
+uswds.paths.dist.img = './assets/img';
+uswds.paths.dist.js = './assets/js';
 
-// Javascript destination
-const JS_DEST = './assets/js';
+/**
+ * Exports
+ * Add as many as you need
+ */
 
-// Compiled CSS destination
-const CSS_DEST = './assets/css';
-const CSS_DEST2 = './_site/assets/css';
-
-/*
-----------------------------------------
-TASKS
-----------------------------------------
-*/
-
-gulp.task('copy-fontawesome', () => {
-  return gulp.src(`./node_modules/@fortawesome/fontawesome-free/css/all.min.css`)
-  .pipe(gulp.dest(`${CSS_DEST}`));
-});
-
-gulp.task('copy-slick-js', () => {
-  return gulp.src(`./node_modules/slick-carousel/slick/**/**.js`)
-  .pipe(gulp.dest(`${JS_DEST}`));
-});
-
-gulp.task('copy-slick-css', () => {
-  return gulp.src(`./node_modules/slick-carousel/slick/**/**.css`)
-  .pipe(gulp.dest(`${CSS_DEST}`));
-});
-
-gulp.task('copy-uswds-setup', () => {
-  return gulp.src(`${uswds}/scss/theme/**/**`)
-  .pipe(gulp.dest(`${PROJECT_SASS_SRC}`));
-});
-
-gulp.task('copy-uswds-fonts', () => {
-  return gulp.src(`${uswds}/fonts/**/**`)
-  .pipe(gulp.dest(`${FONTS_DEST}`));
-});
-
-gulp.task('copy-uswds-images', () => {
-  return gulp.src(`${uswds}/img/**/**`)
-  .pipe(gulp.dest(`${IMG_DEST}`));
-});
-
-gulp.task('copy-uswds-js', () => {
-  return gulp.src(`${uswds}/js/**/**`)
-  .pipe(gulp.dest(`${JS_DEST}`));
-});
-
-gulp.task('build-sass', function(done) {
-  var plugins = [
-    // Autoprefix
-    autoprefixer(autoprefixerOptions),
-    // Pack media queries
-    // mqpacker({ sort: true }),
-    // Minify
-    cssnano(({ autoprefixer: { browsers: autoprefixerOptions }}))
-  ];
-  return gulp.src([
-      `${PROJECT_SASS_SRC}/*.scss`
-    ])
-    .pipe(replace(
-      /\buswds @version\b/g,
-      'uswds v' + pkg.version
-    ))
-    .pipe(sourcemaps.init({ largeFile: true }))
-    .pipe(sass({
-        includePaths: [
-          `${PROJECT_SASS_SRC}`,
-          `${uswds}/scss`,
-          `${uswds}/scss/packages`,
-        ]
-      }))
-    .pipe(postcss(plugins))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(`${CSS_DEST}`))
-    .pipe(gulp.dest(`${CSS_DEST2}`))
-    .pipe(notify({
-      "sound": "Pop" // case sensitive
-    }));
-});
-
-gulp.task('init', gulp.series(
-  'copy-uswds-setup',
-  'copy-uswds-fonts',
-  'copy-uswds-images',
-  'copy-uswds-js',
-  'build-sass'
-));
-
-gulp.task('watch-sass', function () {
-  gulp.watch(`${PROJECT_SASS_SRC}/**/*.scss`, gulp.series('build-sass'));
-});
-
-gulp.task('watch', gulp.series('build-sass', 'watch-sass'));
-
-gulp.task('default', gulp.series('copy-slick-js', 'copy-slick-css', 'copy-fontawesome', 'watch'));
+exports.init = uswds.init;
+exports.compile = uswds.compile;
